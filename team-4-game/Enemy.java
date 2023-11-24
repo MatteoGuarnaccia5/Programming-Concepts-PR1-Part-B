e339f261ce1a29857d4caf5adfbc8080e40807ff
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-11-24 11:51:57 +0000

    enemy health

--- team-4-game/Enemy.java
+++ team-4-game/Enemy.java
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Enemy extends Actor
 {
+    
+    int health = 10;
     /**
      * Act - do whatever the Enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +17,15 @@ public class Enemy extends Actor
     public void act()
     {
         // Add your action code here.
+        //this will need to be changed to be varibale depending on level
+        //also need to be dependent on 'attack' from player not key press
+        if(Greenfoot.isKeyDown("Q")){
+            health -= 10;
+        }
+        if(health <=0){
+            getWorld().removeObject(this);
+        }
     }
+    
+    
 }
