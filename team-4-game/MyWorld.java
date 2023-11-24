6200cd686ae7645be8b624e50210f059d6a8e808
William-Brown5515 <william.brown@stfc.ac.uk>
2023-11-24 10:50:24 +0000

    PLayer moving

--- team-4-game/MyWorld.java
+++ team-4-game/MyWorld.java
@@ -16,9 +16,9 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(1000, 600, 1);
         Enemy enemy1 = new Enemy();
-        player player = new player();
+        player player = new player(30, 30);
         prepare();
     }
     /**
@@ -29,7 +29,7 @@ public class MyWorld extends World
     {
         Enemy enemy = new Enemy();
         addObject(enemy,398,182);
-        player player = new player();
+        player player = new player(30, 30);
         addObject(player,137,187);
     }
 }
--- team-4-game/player.java
+++ team-4-game/player.java
@@ -12,8 +12,49 @@ public class player extends Actor
      * Act - do whatever the player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    Integer width;
+    Integer height;
+    Integer speed = 2;
+    
+    public player(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        GreenfootImage image = getImage();
+        image.scale(35,50);
+        setImage(image);
+    }
+    
     public void act()
     {
-        // Add your action code here.
+        move();
+    }
+    
+    public void move()
+    {
+        int x = getX();
+        int y = getY();
+        int halfWidth = width / 2;
+        
+        if (Greenfoot.isKeyDown("down") && !isAtEdge())
+        {
+            y += speed;
+        }
+        
+        if (Greenfoot.isKeyDown("up") && y > speed)
+        {
+            y -= speed;
+        }
+        
+        if (Greenfoot.isKeyDown("right") && !isAtEdge())
+        {
+            x += speed;
+        }
+        
+        if (Greenfoot.isKeyDown("left") && x > speed)
+        {
+            x -= speed;
+        }
+        setLocation(x, y);
     }
 }
