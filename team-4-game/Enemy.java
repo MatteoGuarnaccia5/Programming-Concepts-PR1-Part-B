f037c3cc82526efca545f75c8824e15617d22eb6
YufanKambang <yufan.kambang@stfc.ac.uk>
2023-12-01 09:39:43 +0000

    enemy chases after player

--- team-4-game/Enemy.java
+++ team-4-game/Enemy.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
 
 /**
  * Write a description of class Enemy here.
@@ -12,8 +13,25 @@ public class Enemy extends Actor
      * Act - do whatever the Enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    player player;
+    public Enemy(player main_player)
+    {
+        player = main_player;
+    }
     public void act()
     {
         // Add your action code here.
+        //move_around();
+        follow();
     }
-}
+    public void move_around()
+    {
+        move(1);
+        turnTowards(getWorld().getWidth()/2, getWorld().getHeight()/2);
+    }
+    public void follow()
+    {
+        move(1);
+        turnTowards(player.getX(), player.getY());
+    }
+}   
--- team-4-game/MyWorld.java
+++ team-4-game/MyWorld.java
@@ -13,12 +13,13 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    public player main_player = new player();
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
-        Enemy enemy1 = new Enemy();
-        player player = new player();
+        Enemy enemy1 = new Enemy(main_player);
+        addObject(main_player, getWidth()/3, getHeight()/3);
         prepare();
     }
     /**
@@ -27,9 +28,8 @@ public class MyWorld extends World
      */
     private void prepare()
     {
-        Enemy enemy = new Enemy();
+        Enemy enemy = new Enemy(main_player);
         addObject(enemy,398,182);
-        player player = new player();
-        addObject(player,137,187);
     }
+    public void spawn_enemy(){}
 }
